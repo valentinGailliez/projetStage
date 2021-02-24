3f22ee28e91e4de69bad47bd64592572df1a7503
valentinGailliez <63590566+valentinGailliez@users.noreply.github.com>
2021-02-24 13:51:21 +0100

    controller correction

--- src/Controller/SkillController.php
+++ src/Controller/SkillController.php
@@ -8,68 +8,63 @@ use App\Form\SkillFormType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class SkillController extends AbstractController
 {
-    private $em, $new, $listSkill, $skill;
+    private $em;
     public function __construct(EntityManagerInterface $entityManager)
     {
         $this->em = $entityManager;
-        $this->listSkill = $this->em->getRepository(Skill::class)->findAll();
-        $this->new = false;
-        $this->skill = new Skill();
     }
 
-    public function getListSkill(Request $request): Response
+    /**
+     * @Route("/skill",name="accueilSkill")
+     */
+    public function list(): Response
     {
-        if ($this->isEmptyList($this->listSkill)) {
-            return $this->createSkill($request);
-        } else {
-            return $this->render('skill/listSkill.html.twig', [
-                'skills' => $this->listSkill
-            ]);
-        }
-    }
-
-
-    public function createSkill(Request $request): Response
-    {
-        if ($this->listSkill == null) {
-            $this->new == true;
-        }
-
-
-        $form = $this->createForms($this->skill, $request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            if ($this->checkForm($this->skill)) {
-                $this->em->persist($this->skill);
-                $this->em->flush();
-                $this->addFlash("success", "La compétence a bien été créée.");
-                return $this->redirectToRoute("accueilSkill");
-            }
-        }
-        return $this->render('skill/createSkill.html.twig', [
-            'form' => $form->createView(),
-            'new' => $this->new
+        $skills = $this->em->getRepository(Skill::class)->findAll();
+        return $this->render('skill/listSkill.html.twig', [
+            'skills' => $skills
         ]);
     }
 
-    public function updateSkill(Request $request): Response
+    /**
+     * @Route("/skill/create",name="createSkill")
+     */
+    public function create(Request $request): Response
     {
-        $id = $request->get('id');
-        $skill = $this->em->getRepository(Skill::class)->findOneBy(['id' => $id]);
+        $skill = new Skill();
+        $form = $this->createForm(SkillFormType::class, $skill);
 
-        $form = $this->createForms($skill, $request);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $this->em->persist($skill);
+            $this->em->flush();
+            $this->addFlash("success", "La compétence a bien été créée.");
+            return $this->redirectToRoute("accueilSkill");
+        }
+        return $this->render('skill/createSkill.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+    /**
+     * @Route("/skill/update/{id}",name="updateSkill")
+     */
+    public function update(Skill $skill, Request $request): Response
+    {
+        $form = $this->createForm(SkillFormType::class, $skill);
+
+        $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
 
-            if ($this->checkForm($skill)) {
-                $this->em->flush();
-                $this->addFlash("success", "La compétence a bien été modifiée.");
-                return $this->redirectToRoute("accueilSkill");
-            }
+
+            $this->em->flush();
+            $this->addFlash("success", "La compétence a bien été modifiée.");
+            return $this->redirectToRoute("accueilSkill");
         }
         return $this->render('skill/updateSkill.html.twig', [
             'form' => $form->createView(),
@@ -77,42 +72,14 @@ class SkillController extends AbstractController
         ]);
     }
 
-    public function deleteSkill(Request $request): Response
+    /**
+     * @Route("/skill/delete/{id}",name="deleteSkill")
+     */
+    public function delete(Skill $skill): Response
     {
-        $id =  $request->get('id');
-        $skill =  $this->em->getRepository(Skill::class)->findOneBy(['id' => $id]);
-        $subskills = $this->em->getRepository(SubSkill::class)->findBy(['skill' => $skill->getId()]);
-
-        if ($this->isEmptyList($subskills)) $this->em->remove($subskills);
         $this->em->remove($skill);
         $this->em->flush();
         $this->addFlash("success", "La compétence a bien été supprimée");
         return $this->redirectToRoute('accueilSkill');
-    }
-
-
-    private function checkForm($skill)
-    {
-        foreach ($this->listSkill as $elementSkill) {
-            if ($elementSkill->getSkillNumber() == $skill->getSkillNumber() && $elementSkill->getSection() == $skill->getSection() && !($elementSkill->getId() == $skill->getId())) {
-
-                $this->addFlash("danger", "Cette identifiant de compétence est déjà attribué pour cette section");
-                return false;
-            }
-        }
-        return true;
-    }
-
-    private function isEmptyList($list)
-    {
-        return $list == null;
-    }
-
-    private function createForms($item, $request)
-    {
-        $form = $this->createForm(SkillFormType::class, $item);
-
-        $form->handleRequest($request);
-        return $form;
     }
 }
--- src/Controller/SubSkillController.php
+++ src/Controller/SubSkillController.php
@@ -14,107 +14,78 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class SubSkillController extends AbstractController
 {
 
-    private $em, $isNew;
+    private $em;
     public function __construct(EntityManagerInterface $entityManagerInterface)
     {
         $this->em = $entityManagerInterface;
-        $this->isNew = true;
     }
     /**
-     * @Route("/sub/skill", name="sub_skill")
+     * @Route("/subskill/{id}", name="accueilSubSkill")
      */
-    public function getSubSkill(Request $request): Response
+    public function list(Skill $skill): Response
     {
-        $id = $request->get("id");
 
-        $skill = $this->em->getRepository(Skill::class)->findOneBy(['id' => $id]);
-
-        $subSkills = $this->em->getRepository(SubSkill::class)->findBy(['skill' => $id]);
-        if ($subSkills == null) {
-            return $this->createSubSkill($request, true);
-        }
         return $this->render('sub_skill/listSubSkill.html.twig', [
-            'subskills' => $subSkills,
             'skill' => $skill
         ]);
     }
-    public function createSubSkill(Request $request): Response
-    {
-        $id = $request->get("id");
-        $subSkills = $this->em->getRepository(SubSkill::class)->findBy(['skill' => $id]);
-        if ($subSkills != null) {
-            $this->isNew = false;
-        }
-        $skill = $this->em->getRepository(Skill::class)->findOneBy(['id' => $id]);
 
+    /**
+     * @Route("/subskill/{id}/create",name="createSubSkill")
+     */
+    public function create(Skill $skill, Request $request): Response
+    {
         $subSkill = new SubSkill();
 
         $form = $this->createForm(SubSkillFormType::class, $subSkill);
 
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
-            if ($this->checkForm($subSkills, $subSkill)) {
-                $subSkill->setSkill($skill);
-                $this->em->persist($subSkill);
-                $this->em->flush();
-                return $this->redirectToRoute("accueilSubSkill", [
-                    'id' => $id
-                ]);
-            }
+
+            $subSkill->setSkill($skill);
+            $this->em->persist($subSkill);
+            $this->em->flush();
+            return $this->redirectToRoute("accueilSubSkill", [
+                'id' => $skill->getId()
+            ]);
         }
         return $this->render('sub_skill/createSubSkill.html.twig', [
             'form' => $form->createView(),
-            'skill' => $skill,
-            'new' => $this->isNew
+            'skill' => $skill
         ]);
     }
-
-    public function deleteSubSkill(Request $request)
+    /**
+     * @Route("/subskill/{id}/delete", name="deleteSubSkill")
+     */
+    public function delete(SubSkill $subSkill)
     {
-        $id = $request->get("idSubSkill");
-        $subskill = $this->em->getRepository(SubSkill::class)->findOneBy(['id' => $id]);
-        if ($subskill != null) {
-            $this->em->remove($subskill);
-            $this->em->flush();
-        }
+        $this->em->remove($subSkill);
+        $this->em->flush();
 
         return $this->redirectToRoute("accueilSubSkill", [
-            'id' => $request->get('id')
+            'id' => $subSkill->getSkill()->getId()
         ]);
     }
 
-    public function updateSubSkill(Request $request)
+    /**
+     * @Route("/subskill/{id}/update",name="updateSubSkill")
+     */
+    public function update(SubSkill $subSkill, Request $request)
     {
-        $id = $request->get("id");
 
-        $listSubSkill = $this->em->getRepository(SubSkill::class)->findBy(['skill' => $id]);
-
-        $subSkill = $this->em->getRepository(SubSkill::class)->findOneBy(['id' => $request->get('idSubSkill')]);
         $form = $this->createForm(SubSkillFormType::class, $subSkill);
 
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
 
-            if ($this->checkForm($listSubSkill, $subSkill)) {
-                $this->em->flush();
-                return $this->redirectToRoute("accueilSubSkill", [
-                    'id' => $id
-                ]);
-            }
+
+            $this->em->flush();
+            return $this->redirectToRoute("accueilSubSkill", [
+                'id' => $subSkill->getSkill()->getId()
+            ]);
         }
         return $this->render('sub_skill/updateSubSkill.html.twig', [
             'form' => $form->createView()
         ]);
-    }
-
-    private function checkForm($list, $itemToCheck)
-    {
-        foreach ($list as $element) {
-            if ($element->getNumber() == $itemToCheck->getNumber() && !($element->getId() == $itemToCheck->getId())) {
-                $this->addFlash("danger", "Cette identifiant de sous-compétence est déjà attribué");
-                return false;
-            }
-        }
-        return true;
     }
 }
